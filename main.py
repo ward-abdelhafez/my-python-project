747c0e779296e65e32681a3c2b4248c18a4fcddf
ward-abdelhafez <36709356+ward-abdelhafez@users.noreply.github.com>
2025-05-23 17:30:44 +0200

    Update main.py
    Third commit, done through GitHub

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello, GitHub! This is my first PyCharm project.")
 def greet(name):
     return f"Welcome, {name}!"
-print(greet("Ward"))
+print(greet("Ward abdelhafez")) #added this comment on GitHub
